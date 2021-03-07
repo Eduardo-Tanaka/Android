1ff304eca441b012a29272a6e56fa0a48b0d2a58
Eduardo <eduardo.s.tanaka@gmail.com>
2021-03-07 16:32:03 -0300

    layout dados cliente

--- app/src/main/java/br/com/eduardotanaka/maximatech/ui/cliente/DadosFragment.kt
+++ app/src/main/java/br/com/eduardotanaka/maximatech/ui/cliente/DadosFragment.kt
@@ -5,20 +5,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.eduardotanaka.maximatech.R
+import br.com.eduardotanaka.maximatech.databinding.FragmentClienteBinding
+import br.com.eduardotanaka.maximatech.util.DateTimeUtil.snackDateTimeFormatter
+import com.google.android.material.snackbar.Snackbar
 import dagger.android.support.DaggerFragment
+import org.threeten.bp.LocalDateTime
 
 class DadosFragment : DaggerFragment() {
+
+    // Scoped to the lifecycle of the fragment's view (between onCreateView and onDestroyView)
+    private var fragmentClienteBinding: FragmentClienteBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cliente, container, false)
+        val binding = FragmentClienteBinding.inflate(inflater, container, false)
+        fragmentClienteBinding = binding
+
+        fragmentClienteBinding!!.btnStatus.setOnClickListener {
+            Snackbar.make(
+                binding.root,
+                snackDateTimeFormatter.format(LocalDateTime.now()) + " - Status ativo",
+                Snackbar.LENGTH_LONG
+            )
+                .setAction("FECHAR") {
+                    // Responds to click on the action
+                }
+                .setActionTextColor(context?.resources?.getColor(R.color.button_status)!!)
+                .show()
+        }
+
+        return binding.root
     }
 
     companion object {
         fun newInstance() = DadosFragment()
     }
 
+    override fun onDestroyView() {
+        // Consider not storing the binding instance in a field, if not needed.
+        fragmentClienteBinding = null
+        super.onDestroyView()
+    }
 }
--- app/src/main/java/br/com/eduardotanaka/maximatech/util/DateTimeUtil.kt
+++ app/src/main/java/br/com/eduardotanaka/maximatech/util/DateTimeUtil.kt
@@ -6,9 +6,12 @@ object DateTimeUtil {
 
     private const val DEFAULT_DATE_TIME_FORMAT = "dd/MM/yyyy"
     private const val DEFAULT_LOCAL_DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    private const val DATE_TIME_HH_MM_FORMAT = "dd/MM/yyyy HH:mm"
 
     val defaultFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)
     val localDateTimeFormatter: DateTimeFormatter =
         DateTimeFormatter.ofPattern(DEFAULT_LOCAL_DATE_TIME_FORMAT)
+    val snackDateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern(DATE_TIME_HH_MM_FORMAT)
+
 
 }
